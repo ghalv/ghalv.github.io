41aee69c718f4513f51a3ba478920f0a52ae76d8
Gunnar K. Halvorsen <gunnar@ghalv.no>
2023-05-29 10:53:57 +0200

    fjernet sudo

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,11 @@ passwd gunnar
 echo "Set password for user spam"
 passwd spam
 
+echo "Opening port 443..."
+ufw allow 443 && ufw status verbose >> firewall
+
 echo "Getting .bashrc file..."
 curl -Lo .bashrc https://ghalv.github.io/bashrc #; source ~/.bashrc
-
-echo "Allowing UFW 443..."
-ufw allow 443
 
 echo "Installing necessary packages..."
 apt-get install -y nginx rsync git
@@ -46,9 +46,6 @@ echo "Add the certbot renew command to the cron jobs list"		# Not working!
 
 echo "Set shell to bash"
 chsh -s /bin/bash $USER
-
-echo "Print open ports"
-sudo ufw status verbose >> firewall
 
 echo "Script has finished successfully! Now 1) source .bashrc, 2) disable pw login and 3) hide nginx version."
 echo "By default, Nginx and most other webservers automatically show their version number on error pages. It's a good idea to disable this from happening because if an exploit comes out for your server software, someone could exploit it. Open the main Nginx config file /etc/nginx/nginx.conf and find the line # server_tokens off;. Uncomment it, and reload Nginx."
